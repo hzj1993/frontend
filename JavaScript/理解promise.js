7eb73cad20225e7a8c88ed6ffeb98ebc1603dc56
Jack <sdad288677777@163.com>
2020-05-27 00:47:24 +0800

    update: 理解promise

--- JavaScript/理解promise.js
+++ JavaScript/理解promise.js
@@ -41,6 +41,7 @@ function step() {
         step();
     });
 }
+
 step();
 
 /* ====================================================================================== */
@@ -58,8 +59,168 @@ function promisify(original) {
         });
     };
 }
+
 // var getID = promisify(fetchID);
 // getID('123').then(res => {/* ... */});
+
+/** ============================================================================================================= **/
+
+// 实现一个简单的promise
+
+const PENDING = 'pending';
+const FULFILLED = 'fulfilled';
+const REJECTED = 'rejected';
+
+function MyPromiseSimple(fn) {
+    const self = this;
+    self.value = null;
+    self.error = null;
+    self.state = PENDING;
+    self.resolvedCallbacks = [];
+    self.rejectedCallbacks = [];
+
+    const resolve = value => {
+        setTimeout(function () {
+            if (self.state === PENDING) {
+                self.state = FULFILLED;
+                self.value = value;
+                self.resolvedCallbacks.map(cb => cb(self.value));
+            }
+        });
+    };
+
+    const reject = error => {
+        setTimeout(function () {
+            if (self.state === PENDING) {
+                self.state = REJECTED;
+                self.error = error;
+                self.rejectedCallbacks.map(cb => cb(self.error));
+            }
+        });
+    };
+
+    fn(resolve, reject);
+}
+
+MyPromiseSimple.prototype.then = function (onFulfilled, onRejected) {
+    const self = this;
+    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : v => v;
+    onRejected = typeof onRejected === 'function' ? onRejected : r => {
+        throw r;
+    };
+    if (self.state === PENDING) {
+        self.resolvedCallbacks.push(onFulfilled);
+        self.rejectedCallbacks.push(onRejected);
+    } else if (self.state === FULFILLED) {
+        onFulfilled(self.value);
+    } else if (self.state === REJECTED) {
+        onRejected(self.error);
+    }
+    return self;
+};
+
+/** ============================================================================================================== **/
+
+// 实现一个符合 Promise/A+ 规范的 Promise
+// Promise/A+ 规范地址：https://www.ituring.com.cn/article/66566
+
+function MyPromise(fn) {
+    const self = this;
+    self.state = PENDING;
+    self.value = null;
+    self.resolvedCallbacks = [];
+    self.rejectedCallbacks = [];
+
+    function resolve(value) {
+        // 如果resolve的参数是promise对象，返回它
+        if (value instanceof MyPromise) {
+            return value.then(resolve, reject);
+        }
+        setTimeout(() => {
+            if (self.state === PENDING) {
+                self.state = FULFILLED;
+                self.value = value;
+                self.resolvedCallbacks.map(cb => cb(self.value));
+            }
+        }, 0);
+    }
+
+    function reject(error) {
+        setTimeout(() => {
+            if (self.state === PENDING) {
+                self.state = REJECTED;
+                self.value = error;
+                self.rejectedCallbacks.map(cb => cb(self.value));
+            }
+        }, 0);
+    }
+
+    try {
+        fn(resolve, reject);
+    } catch (e) {
+        reject(e);
+    }
+}
+
+MyPromise.prototype.then = function (onFulfilled, onRejected) {
+    const self = this;
+    let promise2;
+    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : val => val;
+    onRejected = typeof onRejected === 'function' ? onRejected : err => {
+        throw err;
+    };
+    if (self.state === PENDING) {
+        return (promise2 = new MyPromise((resolve, reject) => {
+            self.resolvedCallbacks.push(() => {
+                try {
+                    const x = onFulfilled(self.value);
+                    resolutionProcedure(promise2, x, resolve, reject);
+                } catch (e) {
+                    reject(e);
+                }
+            });
+            self.rejectedCallbacks.push(() => {
+                try {
+                    const x = onRejected(self.value);
+                    resolutionProcedure(promise2, x, resolve, reject);
+                } catch (e) {
+                    reject(e);
+                }
+            });
+        }));
+    }
+};
+
+function resolutionProcedure(promise2, x, resolve, reject) {
+    if (promise2 === x) {
+        return reject(new TypeError('error'));
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
